a9f23e16b4dd930ba5ecd4f13468363a41c9b867
ayeshagull811 <agull6414@gmail.com>
2025-10-13 11:32:03 +0500

    Fix Tailwind and Node version issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
